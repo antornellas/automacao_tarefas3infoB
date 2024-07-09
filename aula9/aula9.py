a6af67134f03579e0b9a31585258c8c0769718df
Antonella <antonellaornelas4@gmail.com>
2024-07-09 15:02:24 -0300

    aula9

--- aula9/aula9.py
+++ aula9/aula9.py
@@ -1,3 +1,14 @@
 import pyautogui
 
-pyautogui.click(200,200, interval=0.5)]
+pyautogui.click(232,410, interval=0.5)
+pyautogui.press('space')
+
+while True:
+   print(pyautogui.pixel(232,410))
+   if pyautogui.pixelMatchesColor(232,410,[83,83,83],70):
+        pyautogui.press('space')
+        
+   # elif pyautogui.pixelMatchesColor(230,395,[83,83,83],70):
+    #    print(pyautogui.pixel(232,395))
+    #    pyautogui.press('down',interval=2)
+     
