2823b2039724bd4a9c1b5effc2e906262e78b729
Antonella <antonellaornelas4@gmail.com>
2024-08-15 15:49:24 -0300

    aula

--- aula9/aula9.py
+++ aula9/aula9.py
@@ -7,8 +7,7 @@ while True:
    print(pyautogui.pixel(232,410))
    if pyautogui.pixelMatchesColor(232,410,[83,83,83],70):
         pyautogui.press('space')
-        
-   # elif pyautogui.pixelMatchesColor(230,395,[83,83,83],70):
-    #    print(pyautogui.pixel(232,395))
-    #    pyautogui.press('down',interval=2)
-     
+
+   elif pyautogui.pixelMatchesColor(230,395,[83,83,83],70):
+        print(pyautogui.pixel(232,395))
+        pyautogui.press('down',interval=2)        
---
+++ prova_antonelaornelas/calculadora.py
@@ -0,0 +1,11 @@
+import pyautogui
+
+pyautogui.click(770,1050,duration = 1 )
+pyautogui.write ('Calculadora')
+pyautogui.click(825,420,duration = 1 )
+
+pyautogui.click(880,380,duration = 1 )
+pyautogui.click(900,580,duration = 1 )
+pyautogui.click(1150,450,duration = 1 )
+pyautogui.write ('255')
+pyautogui.click(880,560,duration = 1 )
